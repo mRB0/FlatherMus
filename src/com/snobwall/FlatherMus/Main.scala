dff7d32b88abcaea71a84588a05fd2a72c624493
Mike Burke <mrburke@gmail.com>
2010-12-28 19:06:07 -0400

    Extend dynamic class loading to actually find the classes to load

--- src/com/snobwall/FlatherMus/Main.scala
+++ src/com/snobwall/FlatherMus/Main.scala
@@ -10,14 +10,12 @@ object Main {
     val logger : Logger = Logger.getLogger("com.snobwall.FlatherMus");
     
     def main(args : Array[String]) : Unit = {
-        var handlers = Logger.getLogger("").getHandlers;
-        handlers.foreach(handler => handler.setLevel(Level.FINEST));
-        
+        var handlers = Logger.getLogger("").getHandlers()
+        handlers.foreach(handler => handler.setLevel(Level.FINEST))
         logger.setLevel(Level.FINEST);
         
-        logger.log(Level.WARNING, "ServiceEntrypoint is a whatever")
-        logger.log(Level.FINEST, "ServiceEntrypoint is a whatever")
-        var locator = ServiceLocator getInstance
+        var locator = ServiceLocator.getInstance()
+        
         
         
     }
